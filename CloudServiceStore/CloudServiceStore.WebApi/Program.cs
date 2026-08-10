53643991ca244fe697e4d39803bd57c46b458a51
buidi2004 <buidi7170@gmail.com>
2026-08-10 07:15:01 +0700

    feat: Add Docker support and Swagger UI

--- CloudServiceStore/CloudServiceStore.WebApi/Program.cs
+++ CloudServiceStore/CloudServiceStore.WebApi/Program.cs
@@ -21,6 +21,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddRateLimiter(options =>
 {
@@ -81,6 +82,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseMiddleware<CloudServiceStore.WebApi.Middlewares.ExceptionHandlingMiddleware>();
